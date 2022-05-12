c3e8a4bfb9d85cb2950b932c7d8f0bf56e705973
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-05-12 09:57:52 +0200

    .

--- Practica.py
+++ Practica.py
@@ -2,5 +2,6 @@ import pandas as pd
 
 df= pd.read_csv("navegacion", ",")
 df1=df["url_landing"].str.split("[=]", expand=True)
+df1.columns["gclid", "user", "uuid", "camp", "adg", "device", "sl", "adv"]
 
-
+df.duplicate()
